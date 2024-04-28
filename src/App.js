b28770bf297f5a87cb7cbb839c4e28fac769e8bc
ash1198 <ash.ranjan09@gmail.com>
2024-04-28 17:59:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ToggleTheme from './toggleTheme/ToggleTheme'
+
+const App = () => {
+    return (
+        <div>
+            <ToggleTheme />
+        </div>
+    )
+}
+
+export default App
+
